78ff7b8fe16a8d11e249a414e05aa1ab1c368273
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2025-05-05 15:02:41 +0200

    feat(): byte[] Convert to Image

---
+++ App/Helper/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Helper
+{
+    public class ByteArrayToImageConverter : IValueConverter
+    {
+        // Convertit un byte[] en ImageSource
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte[] bytes && bytes.Length > 0)
+            {
+                return ImageSource.FromStream(() => new MemoryStream(bytes));
+            }
+            // Image par défaut si pas de données
+            return ImageSource.FromFile("a0.png");
+        }
+
+        // Pas d’opération inverse nécessaire
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
